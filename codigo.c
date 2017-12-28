996676f6213f146b14f69c64cf679fe900be2efe
ineesgoncalvees <ines98goncalves@gmail.com>
2017-12-28 16:01:28 +0000

    Mostrar mundo

--- codigo.c
+++ codigo.c
@@ -7,7 +7,7 @@ struct agente_inf {
 	unsigned int tipo;
 	unsigned int jogavel;
 	unsigned int id;
-} agente[100];
+} agente[400];
 
 // Define o mundo
 typedef struct {
@@ -16,8 +16,10 @@ typedef struct {
 } MUNDO;
 
 int main() {
-	unsigned int xdim = 10;
-	unsigned int ydim = 10;
+	unsigned int xdim = 20;
+	unsigned int ydim = 20;
+	unsigned int contador = 0;
+	unsigned int contador2 = 0;
 
 
 	// N. de personagens
@@ -33,19 +35,20 @@ int main() {
 			unsigned int tipo = 0;
 			unsigned int jogavel = 0;
 			unsigned int id = 0;
+			contador++;
 
 			// Define a probabilidade
 			unsigned char probabilidade = rand() % 100;
 
-			if (probabilidade < 10 && nagentes < 20) {
+			if (probabilidade < 5 && nagentes < 20) {
 				tipo = (rand() % 2 == 0) ? 1 : 2;
 
 				id = nagentes++;
 			}
 
-			agente[nagentes].tipo = tipo;
-			agente[nagentes].jogavel = jogavel;
-			agente[nagentes].id = id;
+			agente[contador].tipo = tipo;
+			agente[contador].jogavel = jogavel;
+			agente[contador].id = id;
 		}
 
 	}
@@ -59,25 +62,29 @@ int main() {
 		
 		for (unsigned int yi = 0; yi < ydim; yi++) {
 
-			// unsigned int celula = grelha_agente[xi][yi];
+			contador2++;
+			unsigned int celula = agente[contador2].tipo;
 
-			// switch (celula) {
 
-			// 	case 0:
-			// 		printf(" . ");
-			// 		break;
+			switch (celula) {
 
-			// 	case 1:
-			// 		printf("H");
-			// 		break;
+				case 0:
+					printf(" .   ");
+					break;
 
-			// 	case 2:
-			// 		printf("Z");
-			// 		break;
+				case 1:
+					printf(" H");
+					printf("%02x ", agente[contador2].id);
+					break;
 
-			// 	default:
-			// 		printf(" ? ");
-			// }
+				case 2:
+					printf(" Z");
+					printf("%02x ", agente[contador2].id);
+					break;
+
+				default:
+					printf(" ? ");
+			}
 		}
 
 		printf("\n\n");
